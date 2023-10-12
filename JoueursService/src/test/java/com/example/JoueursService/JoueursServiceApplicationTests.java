334ea95f96b38e318ab1b14ee00c0f070aaecf9d
dimitri <dimitri.dd@outlook.fr>
2023-10-12 09:05:42 +0200

    first commit

---
+++ JoueursService/src/test/java/com/example/JoueursService/JoueursServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JoueursService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoueursServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
